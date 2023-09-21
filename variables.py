fe594b9414e6e303e96721104841fe1a1215836b
Eduardo Davila <ortizgomezjassmin@gmail.com>
2023-09-20 22:13:04 -0600

    Create variables.py
    First commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+names = "Jassmin Alondra"
+last_name = "Ortiz"
+age = 21
+list_shopping = ("Milk", "Eggs", "Vegetables")
+
+print(f"mi nombre es {names} {last_name} y tengo {age} años de edad")
+
+
+print(list_shopping)
